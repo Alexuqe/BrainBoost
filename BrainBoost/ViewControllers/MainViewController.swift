4e552f676802543e4832413d78949006cb1d3cc3
alex <hordiah1@google.com>
2025-05-11 22:45:48 +0300

    create backgoround from game buttons

--- BrainBoost/ViewControllers/MainViewController.swift
+++ BrainBoost/ViewControllers/MainViewController.swift
@@ -15,6 +15,15 @@ final class MainViewController: UIViewController {
         return view
     }()
 
+    private let gameButtonsBackground: UIView = {
+        let view = UIView()
+        view.backgroundColor = .buttonsBackground
+        view.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
+        view.layer.cornerRadius = 30
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .main
@@ -26,12 +35,12 @@ final class MainViewController: UIViewController {
     private func setupLayout() {
         view.addSubview(scoreView)
         view.addSubview(difficultyButtons)
-
+        view.addSubview(gameButtonsBackground)
     }
 
     private func setupConstraints() {
         NSLayoutConstraint.activate([
-            scoreView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 25),
+            scoreView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40),
             scoreView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 60),
             scoreView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -60),
             scoreView.heightAnchor.constraint(equalToConstant: 60),
@@ -39,6 +48,11 @@ final class MainViewController: UIViewController {
             difficultyButtons.topAnchor.constraint(equalTo: scoreView.bottomAnchor, constant: 30),
             difficultyButtons.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             difficultyButtons.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+
+            gameButtonsBackground.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            gameButtonsBackground.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            gameButtonsBackground.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            gameButtonsBackground.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.6)
         ])
     }
 }
